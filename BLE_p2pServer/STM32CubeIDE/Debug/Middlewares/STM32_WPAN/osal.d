b082c3f6558b72f7cd2f15192a5c4639dfa625f7
Rhettkd <rhettkdavis@gmail.com>
2025-12-04 14:26:30 -0600

    Add STM32 BLE p2p firmware

---
+++ BLE_p2pServer/STM32CubeIDE/Debug/Middlewares/STM32_WPAN/osal.d
@@ -0,0 +1,4 @@
+Middlewares/STM32_WPAN/osal.o: \
+ C:/Users/rhettdavis/Desktop/STM32Cube_FW_WB_V1.24.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.c \
+ C:/Users/rhettdavis/Desktop/STM32Cube_FW_WB_V1.24.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h
+C:/Users/rhettdavis/Desktop/STM32Cube_FW_WB_V1.24.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h:
